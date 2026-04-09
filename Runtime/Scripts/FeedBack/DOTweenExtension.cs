5ccb22b0ffd4ac8e191855c9be49103f69a78467
lucasdrpx <150352145+lucasdprx@users.noreply.github.com>
2026-04-09 15:06:17 +0200

    Update DOTweenExtension.cs

--- Runtime/Scripts/FeedBack/DOTweenExtension.cs
+++ Runtime/Scripts/FeedBack/DOTweenExtension.cs
@@ -3,6 +3,8 @@ using DG.Tweening;
 using DG.Tweening.Core;
 using DG.Tweening.Plugins.Options;
 using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Menu.FeedBack
 {
@@ -11,6 +13,16 @@ namespace Menu.FeedBack
         public static TweenerCore<float, float, FloatOptions> DOFontSize(this TextMeshProUGUI text, float targetSize, float duration)
         {
             return DOTween.To(() => text.fontSize, x => text.fontSize = x, targetSize, duration);
+        }
+
+        public static TweenerCore<Color, Color, ColorOptions> DOColor(this TextMeshProUGUI text, Color endValue, float duration)
+        {
+            return DOTween.To(() => text.color, x => text.color = x, endValue, duration);
+        }
+        
+        public static TweenerCore<Color, Color, ColorOptions> DOColor(this Image image, Color endValue, float duration)
+        {
+            return DOTween.To(() => image.color, x => image.color = x, endValue, duration);
         }
     }
 }
